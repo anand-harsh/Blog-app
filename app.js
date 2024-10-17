78d50c1098be1a4eaf86026a398a41ae06851207
Yuvraj <devyuvraj883@gmail.com>
2024-10-17 21:02:49 +0530

    Fixed typo

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const path = require('path'); 
+const path = require('path');
 const app = express();
 
 const PORT = 8000;
 
-app.set('viewengine', 'ejs');
+app.set('view engine', 'ejs');
 app.set('views',path.resolve('./views') );
+
+app.get('/', (req, res)=>{
+  res.render('Home');
+})
 
 app.listen(PORT, ()=>{console.log(`Server started on the port:${PORT}`)});
