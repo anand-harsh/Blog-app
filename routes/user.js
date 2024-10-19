2d4649563a401c64e9aaf59c26975c26583e3dcf
Yuvraj <devyuvraj883@gmail.com>
2024-10-19 20:08:15 +0530

    Added route for signing up

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
 router.get('/sign-in', (req, res)=>{
   res.render('./SignIn');
 })
 
 router.get('/sign-up', (req, res)=>{
-  res.render('./SignUp'); 
+  res.render('./SignUp');
+})
+
+router.post('/sign-up' , async (req, res)=>{
+  const {fullName, password, email} = req.body;
+  await User.create({
+    fullName,
+    password,
+    email
+  });
+  res.redirect('/');
+
 })
 
 module.exports = router;
