8b345d2f0cf33d1d2f5510e11c75307d82196e8f
Yuvraj <devyuvraj883@gmail.com>
2024-10-20 01:10:32 +0530

    Added signin route

--- routes/user.js
+++ routes/user.js
@@ -6,16 +6,24 @@ router.get('/sign-in', (req, res)=>{
   res.render('./SignIn');
 })
 
+router.post('/sign-in', async (req, res)=>{
+  const {email, password} = req.body;
+  const user = await User.matchPassword(email, password);
+  if(user)res.redirect('/');
+
+})
+
 router.get('/sign-up', (req, res)=>{
   res.render('./SignUp');
 })
 
 router.post('/sign-up' , async (req, res)=>{
-  const {fullName, password, email} = req.body;
+  const {fullName, password, email, profileImage} = req.body;
   await User.create({
     fullName,
     password,
-    email
+    email,
+    profileImage
   });
   res.redirect('/');
 
