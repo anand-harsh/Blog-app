5fe84db9ee7d6b50747ac08edf91469428f02157
Yuvraj <devyuvraj883@gmail.com>
2024-10-22 11:28:02 +0530

    Created method for validating token

--- services/authentication.js
+++ services/authentication.js
@@ -10,5 +10,15 @@ function generateToken (user){
 
   }
   const token = JWT.sign(payload, secret, {expiresIn: '24h'});
-  return token ; 
+  return token ;
+}
+
+function validateToken (token){
+  const payload = JWT.verify(token, secret);
+  return payload;
+}
+
+module.exports = {
+  generateToken,
+  validateToken
 }
