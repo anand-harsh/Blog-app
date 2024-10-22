06b98cfd80796d7e65439f40f8dc7993655003d7
Yuvraj <devyuvraj883@gmail.com>
2024-10-23 01:55:57 +0530

    Added middlewares for cookie-parsing & auth token validation

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const app = express();
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+
+const userRoute = require('./routes/user');
+const { checkForAuthenticationCookie } = require('./middleware/authentication');
+
+app.use(express.urlencoded({extended:false}))
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie('token'));
+
+mongoose.connect(process.env.MONGO_URI).then(()=>console.log("Mongo DB connected")).catch(error=>console.log(error));
 
 const PORT = 8000;
 
 app.set('view engine', 'ejs');
 app.set('views',path.resolve('./views') );
+
+app.use('/user', userRoute);
 
 app.get('/', (req, res)=>{
   res.render('Home');
